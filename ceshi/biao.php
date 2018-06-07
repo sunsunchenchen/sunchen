381175bec26e93da56b15bf8e00d8e121ca69295
sunsunchenchen <674225113@qq.com>
2018-06-07 15:53:42 +0800

    lala

---
+++ ceshi/biao.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<center>
+    <form action="zhan.php"method="post">
+        <table border="2">
+            <tr>
+                <td>登录</td>
+                <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td>密码</td>
+                <td><input type="password" name="pwd"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="登录"></td>
+                <td></td>
+            </tr>
+
+        </table>
+    </form>
+</center>
+</body>
+</html>
